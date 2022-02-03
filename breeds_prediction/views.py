af16ff3f889db75c0cb409ed88790816eb1edb12
ymolodtsov <ymolodtsov.appwell@ancestry.com>
2022-02-03 22:30:24 +0100

    Update the view with logs

--- breeds_prediction/views.py
+++ breeds_prediction/views.py
@@ -1,13 +1,16 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.decorators.http import require_http_methods
 
 from breeds_prediction.ai_logic.breed_prediction import predict_breed_transfer
 
 
 @ensure_csrf_cookie
-@require_http_methods(["POST"])
 def predict_breed(request):
-    image = request.FILES.get('image_to_predict')
-    breed = predict_breed_transfer(image=image.read())
-    return JsonResponse(data={'breed': breed}, status=200)
+    if request.POST:
+        print('predicting the dog breed', flush=True)
+        image = request.FILES.get('image_to_predict')
+        breed = predict_breed_transfer(image=image.read())
+        return JsonResponse(data={'breed': breed}, status=200)
+    else:
+        print('not a POST request', flush=True)
+        return HttpResponse(status=200)
