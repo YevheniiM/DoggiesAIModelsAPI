1e637590b4f5a20adc22bc1987de6f975a4e4d81
ymolodtsov <ymolodtsov.appwell@ancestry.com>
2022-02-04 21:49:02 +0100

    Add neural model to the cache

--- breeds_prediction/apps.py
+++ breeds_prediction/apps.py
@@ -6,14 +6,12 @@ import torch.nn as nn
 import torchvision.models as models
 from django.apps import AppConfig
 from django.conf import settings
+from django.core.cache import cache
 from django.core.files.storage import default_storage
-from torch import optim
 
 
 class BreedPredictionModel:
     def __init__(self):
-        self.model = models.vgg16()
-
         if settings.DEBUG:
             with open(os.path.join(settings.MODELS_PATH, 'breeds.txt'), 'r') as f:
                 self.class_names = [n.strip() for n in f.readlines()]
@@ -21,25 +19,33 @@ class BreedPredictionModel:
             with default_storage.open(os.path.join(settings.MODELS_PATH, 'breeds.txt'), "r") as f:
                 self.class_names = [n.decode("utf-8").strip() for n in f.readlines()]
 
-        for param in self.model.features.parameters():
-            param.requires_grad = False
+        model_cache_key = 'breed_prediction_model'
+        self.model = cache.get(model_cache_key)
 
-        n_inputs = self.model.classifier[6].in_features
-
-        last_layer = nn.Linear(n_inputs, 133)
-
-        self.model.classifier[6] = last_layer
-        # criterion_transfer = nn.CrossEntropyLoss()
-        # optimizer_transfer = optim.SGD(self.model.classifier.parameters(), lr=0.001)
-
-        if settings.DEBUG:
-            print(f"DEBUG: Initializing model [{os.path.join(settings.MODELS_PATH, 'breed_prediction.pt')}]")
-            with default_storage.open(os.path.join(settings.MODELS_PATH, 'breed_prediction.pt'), "rb") as f:
-                self.model.load_state_dict(torch.load(io.BytesIO(f.read())))
+        if self.model:
+            print("Found model in the cache, skipping loading...")
         else:
-            print(f"INFO: Initializing model [{os.path.join(settings.MODELS_PATH, 'breed_prediction.pt')}]")
-            with default_storage.open(os.path.join(settings.MODELS_PATH, 'breed_prediction.pt'), "rb") as f:
-                self.model.load_state_dict(torch.load(io.BytesIO(f.read())))
+            print("Haven't found model in the cache, loading...")
+
+            self.model = models.vgg16()
+
+            for param in self.model.features.parameters():
+                param.requires_grad = False
+
+            n_inputs = self.model.classifier[6].in_features
+            last_layer = nn.Linear(n_inputs, 133)
+            self.model.classifier[6] = last_layer
+
+            if settings.DEBUG:
+                print(f"DEBUG: Initializing model [{os.path.join(settings.MODELS_PATH, 'breed_prediction.pt')}]")
+                with default_storage.open(os.path.join(settings.MODELS_PATH, 'breed_prediction.pt'), "rb") as f:
+                    self.model.load_state_dict(torch.load(io.BytesIO(f.read())))
+            else:
+                print(f"INFO: Initializing model [{os.path.join(settings.MODELS_PATH, 'breed_prediction.pt')}]")
+                with default_storage.open(os.path.join(settings.MODELS_PATH, 'breed_prediction.pt'), "rb") as f:
+                    self.model.load_state_dict(torch.load(io.BytesIO(f.read())))
+
+            cache.set(model_cache_key, self.model, None)
 
 
 class BreedsPredictionConfig(AppConfig):
@@ -50,4 +56,3 @@ class BreedsPredictionConfig(AppConfig):
     class_names = init_model.class_names
 
     # if os.environ.get('RUN_MAIN', None) != 'true':
-
